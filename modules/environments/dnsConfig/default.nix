ff94edf6065cfe37747b804458a5c8cfb3ac6142
Jakob Lauterbach <j.lauterbach@ingburo.de>
2026-04-10 17:56:35 +0200

    fix: define regex wihtout \d

--- modules/environments/dnsConfig/default.nix
+++ modules/environments/dnsConfig/default.nix
@@ -8,7 +8,7 @@ in
     enable = mkEnableOption "Local Network DNS Server + Reverse Proxy";
 
     ipAddress = mkOption {
-      type = types.strMatching "^\d+\.\d+\.\d+\.\d+$";
+      type = types.strMatching "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$";
       default = null;
       example = "192.168.0.50";
       description = "The IP Address to route resolve hostnames to.";
