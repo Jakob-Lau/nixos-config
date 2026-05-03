86c999ce274ba555db5c35629bb6c55b47dd3610
Jakob Lauterbach <j.lauterbach@ingburo.de>
2026-05-03 19:59:51 +0200

    feat: add basic homepage setup

---
+++ modules/environments/homepage/default.nix
@@ -0,0 +1,55 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.my.profiles.homepage;
+  dashboardPort = 8082;
+  dashboardHost = "dashboard";
+in
+{
+  options.my.homepage.services = with lib; {
+    enable = mkEnableOption "Homepage Dashboard";
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.homepage-dashboard = {
+      enable = true;
+      listenPort = dashboardPort;
+      allowedHosts = "${dashboardHost}.home:${toString dashboardPort},localhost:${toString dashboardPort},127.0.0.1:${dashboardPort}";
+
+      # Services (tiles)
+      services = [
+        {
+          "System" = [
+            {
+              "Resources" = {
+                description = "System metrics";
+              };
+            }
+          ];
+        }
+      ];
+
+      # Widgets
+      widgets = [
+        {
+          resources = {
+            cpu = true;
+            units = "metric";
+            memory = true;
+            disk = "/";
+            network = true;
+          };
+        }
+      ];
+    };
+
+    # add dns entry to dnsmasq
+    my.profiles.dnsConfig.entries = [
+      { name = dashboardHost; port = dashboardPort; }
+    ];
+  };
+}
