3cce2176285f7f376d60c47f5ec6917ae11862f8
Jakob-Lau <59151562+Jakob-Lau@users.noreply.github.com>
2026-07-02 23:42:52 +0200

    chore: update nixpkgs (#4)

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nix.url = "github:NixOS/nix";
-    nixpkgs.url = "nixpkgs/nixos-25.11";
+    nixpkgs.url = "nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
