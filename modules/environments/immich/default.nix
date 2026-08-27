319133af6239a6b789d5acadd111014deb1b11d9
Jakob Lauterbach <j.lauterbach@ingburo.de>
2026-08-27 22:16:43 +0200

    fix: nginx settings without quotes and with semicolon

--- modules/environments/immich/default.nix
+++ modules/environments/immich/default.nix
@@ -34,9 +34,9 @@ in
         name = cfg.dnsEntry;
         port = cfg.port;
         extraConfig = ''
-          client_max_body_size "0"
-          proxy_read_timeout "600s"
-          proxy_send_timeout "600s"
+          client_max_body_size 0;
+          proxy_read_timeout 600s;
+          proxy_send_timeout 600s;
         '';
       }
     ];
