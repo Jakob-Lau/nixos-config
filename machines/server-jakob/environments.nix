55c1fb7d9ed9670db45f5eab2ff3eaf779b57336
Jakob Lauterbach <j.lauterbach@ingburo.de>
2026-08-28 15:22:44 +0200

    fix: define dnsConfig first

--- machines/server-jakob/environments.nix
+++ machines/server-jakob/environments.nix
@@ -5,17 +5,17 @@ let
 in
 {
   my.profiles = {
+    dnsConfig = {
+      enable = true;
+      ipAddress = "192.168.0.96";
+      domain = "jakobsclou.de";
+    };
+
     backup = {
       enable = true;
       device = "/dev/disk/by-uuid/40332bb2-1e4f-4887-add0-867f19048dc8";
       mountPoint = "/mnt/backup";
       schedule = "Sun 03:00";
-    };
-
-    dnsConfig = {
-      enable = true;
-      ipAddress = "192.168.0.96";
-      domain = "jakobsclou.de";
     };
 
     homepage = {
